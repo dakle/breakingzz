c37adb3d3b15db529b6c3e9daa450bf55fe88265
lookamod <53235698+lookamod@users.noreply.github.com>
2019-08-04 17:12:41 +0200

    Update asf50.js

--- asf50.js
+++ asf50.js
@@ -1159,20 +1159,7 @@ client76.on("ready", () => {
 
 client76.login(process.env.S76);
 
-const client78 = new Discord.Client();
 
-client78.on("ready", () => {
-
-	      var interval = setInterval (function () {
-        client78.channels.get("607566145523810319").send(Math.random().toString(36).substring(7)) 
-
-}, 270000);
-
-});
-
-
-
-client78.login(process.env.S78);
 
 client1.on('message', msg => {
     let ownerid = '607348270565556285';
@@ -2690,24 +2677,4 @@ client76.on('message', msg => {
     }
 });
 
-client78.on('message', msg => {
-    let ownerid = '607348270565556285';
-    if(msg.author.id === ownerid){
-        if(msg.content.startsWith('c')){
 
-            if(msg.isMentioned(client78.user)){
-                farm.set(`farm`, 'blah blah')
-                let args = msg.content.split(` `)
-                msg.channel.send(`#credits <@607348270565556285> ${args[2]}`).then(()=>{
-                    const filter = m => m.author.id === msg.author.id;
-                    msg.channel.awaitMessages(filter, {max: 1, time: 29000}).then(col=>{
-                        if(col.first().content === "cancel"){
-                            return msg.reply(`Canceled!`);
-                        }
-                        msg.channel.send(`${col.first().content}`).then(()=> farm.set(`farm`, 'true'))
-                    })
-                })
-            }
-        }
-    }
-});
